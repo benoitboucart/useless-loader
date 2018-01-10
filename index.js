b71f58d5806a2213358f0fe8bfbddfb95a688dec
Benoît Boucart <benoit@webbb.be>
2018-01-10 17:27:32 +0100

    Put comment in head

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-const DOMParser = require('xmldom').DOMParser;
-
 module.exports = function uselessLoader(source, map) {
   console.log("Hello from the useless laoder!");
 
-  var re = /<\/body>(?![\s\S]*<\/body>[\s\S]*$)/i;
-  const output = source.replace(re, '<div style="position: fixed; bottom: 0; left:0; right:0; background: red; color: #FFF; padding: 10px;"><marquee>VEEL SUCCES MET HET EXAMEN!</marquee></div></body>')
+  const re = /<\/head>(?![\s\S]*<\/head>[\s\S]*$)/i;
+  const output = source.replace(re, `
+    <!-- < Veel succes met het examen >
+     ----------------------------------------
+
+
+_______oBBBBB8o______oBBBBBBB
+_____o8BBBBBBBBBBB__BBBBBBBBB8________o88o,
+___o8BBBBBB**8BBBB__BBBBBBBBBB_____oBBBBBBBo,
+__oBBBBBBB*___***___BBBBBBBBBB_____BBBBBBBBBBo,
+_8BBBBBBBBBBooooo___*BBBBBBB8______*BB*_8BBBBBBo,
+_8BBBBBBBBBBBBBBBB8ooBBBBBBB8___________8BBBBBBB8,
+__*BBBBBBBBBBBBBBBBBBBBBBBBBB8_o88BB88BBBBBBBBBBBB,
+____*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB8,
+______**8BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB*,
+___________*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB8*,
+____________*BBBBBBBBBBBBBBBBBBBBBBBB8888**,
+_____________BBBBBBBBBBBBBBBBBBBBBBB*,
+_____________*BBBBBBBBBBBBBBBBBBBBB*,
+______________*BBBBBBBBBBBBBBBBBB8,
+_______________*BBBBBBBBBBBBBBBB*,
+________________8BBBBBBBBBBBBBBB8,
+_________________8BBBBBBBBBBBBBBBo,
+__________________BBBBBBBBBBBBBBB8,
+__________________BBBBBBBBBBBBBBBB,
+
+ -->
+    </head>`)
+
+  // const output = source;
+  // console.log(`%c ________________________________________
+  // < Veel succes met het examen >
+  //  ----------------------------------------
+  //         \\   ^__^
+  //          \\  (oo)\\_______
+  //             (__)\\       )\\/\\
+  //                 ||----w |
+  //                 ||     ||`);
 
   this.callback(null, output, map);
 };
