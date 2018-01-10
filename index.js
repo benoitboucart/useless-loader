16cf16286e5e163b95cab8faf111e6dd498a5026
Benoît Boucart <benoit@webbb.be>
2018-01-10 13:58:20 +0100

    Add text

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const DOMParser = require('xmldom').DOMParser;
+
 module.exports = function uselessLoader(source, map) {
   console.log("Hello from the useless laoder!");
-  this.callback(null, source, map);
+
+  var re = /<\/body>(?![\s\S]*<\/body>[\s\S]*$)/i;
+  const output = source.replace(re, '<div style="position: fixed; bottom: 0; left:0; right:0; background: red; color: #FFF; padding: 10px;"><marquee>VEEL SUCCES MET HET EXAMEN!</marquee></div></body>')
+
+  this.callback(null, output, map);
 };
