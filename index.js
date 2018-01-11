f2d2414c55151d991914425a9fe104dadc9ab3d8
Benoît Boucart <benoit@webbb.be>
2018-01-11 09:31:10 +0100

    Fix asci

--- index.js
+++ index.js
@@ -3,42 +3,35 @@ module.exports = function uselessLoader(source, map) {
 
   const re = /<\/head>(?![\s\S]*<\/head>[\s\S]*$)/i;
   const output = source.replace(re, `
-    <!-- < Veel succes met het examen >
-     ----------------------------------------
+    <!--
+       ██╗   ██╗███████╗███████╗██╗         ███████╗██╗   ██╗ ██████╗ ██████╗███████╗███████╗
+       ██║   ██║██╔════╝██╔════╝██║         ██╔════╝██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝
+       ██║   ██║█████╗  █████╗  ██║         ███████╗██║   ██║██║     ██║     █████╗  ███████╗
+       ╚██╗ ██╔╝██╔══╝  ██╔══╝  ██║         ╚════██║██║   ██║██║     ██║     ██╔══╝  ╚════██║
+        ╚████╔╝ ███████╗███████╗███████╗    ███████║╚██████╔╝╚██████╗╚██████╗███████╗███████║
+         ╚═══╝  ╚══════╝╚══════╝╚══════╝    ╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝╚══════╝╚══════╝
 
-
-_______oBBBBB8o______oBBBBBBB
-_____o8BBBBBBBBBBB__BBBBBBBBB8________o88o,
-___o8BBBBBB**8BBBB__BBBBBBBBBB_____oBBBBBBBo,
-__oBBBBBBB*___***___BBBBBBBBBB_____BBBBBBBBBBo,
-_8BBBBBBBBBBooooo___*BBBBBBB8______*BB*_8BBBBBBo,
-_8BBBBBBBBBBBBBBBB8ooBBBBBBB8___________8BBBBBBB8,
-__*BBBBBBBBBBBBBBBBBBBBBBBBBB8_o88BB88BBBBBBBBBBBB,
-____*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB8,
-______**8BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB*,
-___________*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB8*,
-____________*BBBBBBBBBBBBBBBBBBBBBBBB8888**,
-_____________BBBBBBBBBBBBBBBBBBBBBBB*,
-_____________*BBBBBBBBBBBBBBBBBBBBB*,
-______________*BBBBBBBBBBBBBBBBBB8,
-_______________*BBBBBBBBBBBBBBBB*,
-________________8BBBBBBBBBBBBBBB8,
-_________________8BBBBBBBBBBBBBBBo,
-__________________BBBBBBBBBBBBBBB8,
-__________________BBBBBBBBBBBBBBBB,
-
- -->
+         _______oBBBBB8o______oBBBBBBB
+         _____o8BBBBBBBBBBB__BBBBBBBBB8________o88o,
+         ___o8BBBBBB**8BBBB__BBBBBBBBBB_____oBBBBBBBo,
+         __oBBBBBBB*___***___BBBBBBBBBB_____BBBBBBBBBBo,
+         _8BBBBBBBBBBooooo___*BBBBBBB8______*BB*_8BBBBBBo,
+         _8BBBBBBBBBBBBBBBB8ooBBBBBBB8___________8BBBBBBB8,
+         __*BBBBBBBBBBBBBBBBBBBBBBBBBB8_o88BB88BBBBBBBBBBBB,
+         ____*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB8,
+         ______**8BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB*,
+         ___________*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB8*,
+         ____________*BBBBBBBBBBBBBBBBBBBBBBBB8888**,
+         _____________BBBBBBBBBBBBBBBBBBBBBBB*,
+         _____________*BBBBBBBBBBBBBBBBBBBBB*,
+         ______________*BBBBBBBBBBBBBBBBBB8,
+         _______________*BBBBBBBBBBBBBBBB*,
+         ________________8BBBBBBBBBBBBBBB8,
+         _________________8BBBBBBBBBBBBBBBo,
+         __________________BBBBBBBBBBBBBBB8,
+         __________________BBBBBBBBBBBBBBBB,
+       -->
     </head>`)
-
-  // const output = source;
-  // console.log(`%c ________________________________________
-  // < Veel succes met het examen >
-  //  ----------------------------------------
-  //         \\   ^__^
-  //          \\  (oo)\\_______
-  //             (__)\\       )\\/\\
-  //                 ||----w |
-  //                 ||     ||`);
 
   this.callback(null, output, map);
 };
